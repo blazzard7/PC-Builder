4d4060385f447dd5f5e7d564333b078fc1435e45
Deadlocked <korotayev05@mail.ru>
2024-11-06 00:20:16 +0500

    Добалены crud-операции

--- CRUDAPP/Program.cs
+++ CRUDAPP/Program.cs
@@ -5,7 +5,31 @@ List<PC> repo = [];
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
+List<PC> GetPC() => repo;
+void CreatePC(PC pc)=> repo.Add(pc);
+PC FindPC(int id)=> repo.Find(x => x.Id == id);
+void UpdatePC(PC dto)
 
+
+
+
+{
+    foreach (var i in repo)
+        if(i.Id == dto.Id)
+        {
+            if(i.Screen!= dto.Screen && dto.Screen != "")
+                i.Screen = dto.Screen;
+            if(i.Keyboard!= dto.Keyboard && dto.Keyboard != "")
+                i.Keyboard = dto.Keyboard;
+            if(i.Mouse!= dto.Mouse && dto.Mouse != "")
+                i.Mouse = dto.Mouse;
+            if(i.Processor!= dto.Processor && dto.Processor != "")
+                i.Processor = dto.Processor;
+            if(i.VideoCard!= dto.VideoCard && dto.VideoCard != "")
+                i.VideoCard = dto.VideoCard;
+        }
+}
+void DeletePC(int id)=> repo.Remove(FindPC(id));
 class PC{
     public int Id{get; set;}
     public string Screen{get; set;}
