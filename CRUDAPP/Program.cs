80ebec200c71e6a209750a70395b73018a0afb5d
Deadlocked <korotayev05@mail.ru>
2024-11-06 00:16:20 +0500

    Инициализация

---
+++ CRUDAPP/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+class PC{
+    public int Id{get; set;}
+    public string Screen{get; set;}
+    public string Keyboard{get; set;}
+    public string Mouse{get; set;}
+    public string Processor{get;set;}
+    public string VideoCard{get; set;}
+}
